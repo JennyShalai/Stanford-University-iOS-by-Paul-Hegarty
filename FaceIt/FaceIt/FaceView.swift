660fc57247d74d9a6ceb38e797a5b8dc8dda850c
JennyShalai <jenny.shalai@gmail.com>
2017-02-04 08:30:37 -0800

    add lecture #4

---
+++ FaceIt/FaceIt/FaceView.swift
@@ -0,0 +1,72 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Jenny Shalai on 2/3/17.
+//  Copyright © 2017 shalai. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    // PUBLIC
+    var scale: CGFloat = 0.90
+    
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private struct Ratios {
+        static let SkullRadiusToEyeOffset: CGFloat = 3
+        static let SkullRadiusToEyeRadius: CGFloat = 10
+        static let SkullRadiusToMouthWidth: CGFloat = 1
+        static let SkullRadiusToMouthHeight: CGFloat = 3
+        static let SkullRadiusToMouthOffset: CGFloat = 3
+    }
+    
+    private enum Eye {
+        case Right
+        case Left
+    }
+    
+    private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+        let path = UIBezierPath(
+            arcCenter: midPoint,
+            radius: radius,
+            startAngle: 0.0,
+            endAngle: CGFloat(2*M_PI),
+            clockwise: true
+        )
+        path.lineWidth = 0.5
+        return path
+    }
+    
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
+        let eyeOffset = skullRadius / Ratios.SkullRadiusToEyeOffset
+        var eyeCenter = skullCenter
+        eyeCenter.y -= eyeOffset
+        
+        switch eye {
+        case .Left: eyeCenter.x -= eyeOffset
+        case .Right: eyeCenter.x += eyeOffset
+        }
+        return pathForCircleCenteredAtPoint(midPoint: eyeCenter, withRadius: eyeRadius)
+    }
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        UIColor.blue.set()
+        pathForCircleCenteredAtPoint(midPoint: skullCenter, withRadius: skullRadius).stroke()
+        pathForEye(eye: .Left).stroke()
+        pathForEye(eye: .Right).stroke()
+    }
+   
+
+}
---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Jenny Shalai on 2/3/17.
+//  Copyright © 2017 shalai. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
